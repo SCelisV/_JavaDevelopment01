adf36f3af7f3cf1d9fa36160df7018c8ab7e0420
SCelis <31612724+SCelisV@users.noreply.github.com>
2022-05-02 23:15:41 +0200

    Organizando

--- javasrc-main/src/testing/ExternaInterna_test.java
+++ javasrc-main/src/testing/ExternaInterna_test.java
@@ -5,8 +5,10 @@ public class ExternaInterna_test {
     public static void main(String[] args) {
         
         ExternaClass externa = new ExternaClass();
-        ExternaClass.InternaClass interna = externa.new InternaClass();
-        System.out.println(interna.y + " " + externa.x);		//	5 10
+        System.out.println(externa.x);		//	10
+        
+        ExternaClass.InternaClass interna = externa.new InternaClass();  
+        System.out.println(interna.y);		//	5
 
     }
 }
